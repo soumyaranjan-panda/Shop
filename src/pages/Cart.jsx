f649cd1b9265adee9088f0b7ea017016048f5e32
Soumrnjn <soumyaranjanpanda988@gmail.com>
2023-08-14 21:35:15 +0530

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,6 +5,7 @@ import CartItem from "../components/CartItem";
 
 const Cart = () => {
   const { cart } = useSelector((state) => state);
+
   console.log("Printing Cart");
   console.log(cart);
   const [totalAmount, setTotalAmount] = useState(0);
@@ -12,9 +13,17 @@ const Cart = () => {
   useEffect(() => {
     setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
   }, [cart]);
+  console.log("Hello useeffect rendered");
+  useEffect(()=>{
+    console.log("Hello useeffect rendered");
+  },[0])
+
+  function clickHandler() {
+    setTotalAmount("Free Free Free");
+  }
 
   return (
-    <div >
+    <div>
       {cart.length > 0 ? (
         <div className="max-w-[1200px] mx-auto flex flex-col md:flex-row justify-center">
           <div className="w-[100%] md:w-[60%] flex flex-col p-2">
@@ -25,18 +34,49 @@ const Cart = () => {
 
           <div className="w-[100%] md:w-[40%] mt-5  flex flex-col">
             <div className="flex flex-col p-5 gap-5 my-14  h-[100%] justify-between">
-            <div className="flex flex-col gap-5 ">
-            <div className="font-semibold text-xl text-green-800 ">Your Cart</div>
-              <div className="font-semibold text-5xl text-green-700  -mt-5">Summary</div>
-              <p className="text-xl">
-                <span className="text-gray-700 font-semibold text-xl">Total Items: {cart.length}</span>
-              </p>
+              <div className="flex flex-col gap-5 ">
+                <div className="font-semibold text-xl text-green-800 ">
+                  Your Cart
+                </div>
+                <div className="font-semibold text-5xl text-green-700  -mt-5">
+                  Summary
+                </div>
+                <p className="text-xl">
+                  <span className="text-gray-700 font-semibold text-xl">
+                    Total Items: {cart.length}
+                  </span>
+                </p>
+              </div>
             </div>
+
+            <div>
+              {cart.some((p) => p.id == 1 || p.id == 2) ? (
+                <div className="flex flex-col">
+                  <p className="text-xl font-bold">
+                    <span className="text-gray-700 font-semibold">Dicount</span>
+                  </p>
+                  <button
+                    onClick={clickHandler}
+                    className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl"
+                  >
+                    Apply Discount
+                  </button>
+                </div>
+              ) : (
+                ""
+              )}
             </div>
 
             <div className="flex flex-col">
-              <p className="text-xl font-bold"><span className="text-gray-700 font-semibold">Total Amount:</span> ${totalAmount}</p>
-              <button className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl">CheckOut Now</button>
+              <p className="text-xl font-bold">
+                <span className="text-gray-700 font-semibold">
+                  Total Amount:
+                </span>{" "}
+                ${totalAmount}
+              </p>
+              <button className="bg-green-700 hover:bg-purple-50 rounded-lg text-white transition duration-300 ease-linear mt-5 border-2 border-green-600 font-bold hover:text-green-700 p-3 text-xl">
+                CheckOut Now
+              </button>
             </div>
           </div>
         </div>
